9375ac85783b7b588c22bc2bfb0f3dd4855b8c60
IYurij <120470454+IYurij@users.noreply.github.com>
2023-10-01 20:18:09 +0700

    Small fixes

--- src/InputData.java
+++ src/InputData.java
@@ -1,31 +1,19 @@
 /**
  * Class for get some user data and return object
- * @version 0.1
  */
 public class InputData {
-    private String text;
-    private int repeatCount;
+    private final String text;
+    private final int repeatCount;
 
-    /**
-     * Class constructor
-     */
-    InputData(String text, int repeatCount) {
+    public InputData(String text, int repeatCount) {
         this.text = text;
         this.repeatCount = repeatCount;
     }
 
-    /**
-     * Getter for text field
-     * @return text
-     */
     public String getText() {
         return text;
     }
 
-    /**
-     * Getter for repeatCount
-     * @return repeatCount
-     */
     public int getRepeatCount() {
         return repeatCount;
     }
